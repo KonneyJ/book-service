7da7826afa97de362aaca88ff1cec71393bc2b70
KonneyJ <konnova.j@yandex.ru>
2025-07-10 15:35:02 +0300

    Добавлены классы Author, Book

---
+++ src/main/java/org/bookservice/model/Author.java
@@ -0,0 +1,7 @@
+package org.bookservice.model;
+
+public class Author {
+    private Long id;
+    private String name;
+    private int birthYear;
+}
---
+++ src/main/java/org/bookservice/model/Book.java
@@ -0,0 +1,9 @@
+package org.bookservice.model;
+
+public class Book {
+    private Long id;
+    private String title;
+    private Long authorId;
+    private int year;
+    private String genre;
+}
